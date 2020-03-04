ec9f1a15c3d22ef66ffa606fe8483d3492c17aff
Andy Williams <andy@andy.xyz>
2020-03-04 18:14:30 +0000

    Correct OSC parsing and logging of unrecognised commands

--- osc.go
+++ osc.go
@@ -3,13 +3,15 @@ package terminal
 import "log"
 
 func (t *Terminal) handleOSC(code string) {
-	if len(code) > 2 && code[1] == ';' {
-		switch code[0] {
-		case '0':
-			t.config.Title = code[2:]
-			t.onConfigure()
-		}
-	} else {
+	if len(code) <= 2 || code[1] != ';' {
+		return
+	}
+
+	switch code[0] {
+	case '0':
+		t.config.Title = code[2:]
+		t.onConfigure()
+	default:
 		log.Println("Unrecognised OSC:", code)
 	}
 }
