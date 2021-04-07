0e3f5e035608a8d92c613edd011cea3cf39fcf42
Andy Williams <andy@andy.xyz>
2021-04-07 23:12:03 +0100

    Complete the color implementation

--- color.go
+++ color.go
@@ -9,6 +9,29 @@ import (
 	"fyne.io/fyne/v2/theme"
 )
 
+var (
+	basicColors = []color.Color{
+		color.Black,
+		&color.RGBA{170, 0, 0, 255},
+		&color.RGBA{0, 170, 0, 255},
+		&color.RGBA{170, 170, 0, 255},
+		&color.RGBA{0, 0, 170, 255},
+		&color.RGBA{170, 0, 170, 255},
+		&color.RGBA{0, 255, 255, 255},
+		&color.RGBA{170, 170, 170, 255},
+	}
+	brightColors = []color.Color {
+		&color.RGBA{85, 85, 85, 255},
+		&color.RGBA{255, 85, 85, 255},
+		&color.RGBA{85, 255, 255, 255},
+		&color.RGBA{255, 255, 85, 255},
+		&color.RGBA{85, 85, 255, 255},
+		&color.RGBA{255, 85, 255, 255},
+		&color.RGBA{85, 255, 255, 255},
+		&color.RGBA{255, 255, 255, 255},
+	}
+)
+
 var currentFG, currentBG color.Color
 
 func (t *Terminal) handleColorEscape(message string) {
@@ -74,72 +97,104 @@ func (t *Terminal) handleColorMode(mode string, bright bool) {
 		} else {
 			currentFG = bg
 		}
-	case "30":
-		if bright {
-			currentFG = &color.RGBA{85, 85, 85, 255}
+	case "30", "90":
+		if bright || mode[0] == '9' {
+			currentFG = brightColors[0]
 		} else {
-			currentFG = color.Black
+			currentFG = basicColors[0]
 		}
-	case "31":
-		if bright {
-			currentFG = &color.RGBA{255, 85, 85, 255}
+	case "31", "91":
+		if bright || mode[0] == '9' {
+			currentFG = brightColors[1]
 		} else {
-			currentFG = &color.RGBA{170, 0, 0, 255}
+			currentFG = basicColors[1]
 		}
-	case "32":
-		if bright {
-			currentFG = &color.RGBA{85, 255, 255, 255}
+	case "32", "92":
+		if bright || mode[0] == '9' {
+			currentFG = brightColors[2]
 		} else {
-			currentFG = &color.RGBA{0, 170, 0, 255}
+			currentFG = basicColors[2]
 		}
-	case "33":
-		if bright {
-			currentFG = &color.RGBA{255, 255, 85, 255}
+	case "33", "93":
+		if bright || mode[0] == '9' {
+			currentFG = brightColors[3]
 		} else {
-			currentFG = &color.RGBA{170, 170, 0, 255}
+			currentFG = basicColors[3]
 		}
-	case "34":
-		if bright {
-			currentFG = &color.RGBA{85, 85, 255, 255}
+	case "34", "94":
+		if bright || mode[0] == '9' {
+			currentFG = brightColors[4]
 		} else {
-			currentFG = &color.RGBA{0, 0, 170, 255}
+			currentFG = basicColors[4]
 		}
-	case "35":
-		if bright {
-			currentFG = &color.RGBA{255, 85, 255, 255}
+	case "35", "95":
+		if bright || mode[0] == '9' {
+			currentFG = brightColors[5]
 		} else {
-			currentFG = &color.RGBA{170, 0, 170, 255}
+			currentFG = basicColors[5]
 		}
-	case "36":
-		if bright {
-			currentFG = &color.RGBA{85, 255, 255, 255}
+	case "36", "96":
+		if bright || mode[0] == '9' {
+			currentFG = brightColors[6]
 		} else {
-			currentFG = &color.RGBA{0, 170, 170, 255}
+			currentFG = basicColors[6]
 		}
-	case "37":
-		if bright {
-			currentFG = &color.RGBA{255, 255, 255, 255}
+	case "37", "97":
+		if bright || mode[0] == '9' {
+			currentFG = brightColors[7]
 		} else {
-			currentFG = &color.RGBA{170, 170, 170, 255}
+			currentFG = basicColors[7]
 		}
 	case "39":
 		currentFG = nil
-	case "40":
-		currentBG = color.Black
-	case "41":
-		currentBG = &color.RGBA{170, 0, 0, 255}
-	case "42":
-		currentBG = &color.RGBA{0, 170, 0, 255}
-	case "43":
-		currentBG = &color.RGBA{170, 170, 0, 255}
-	case "44":
-		currentBG = &color.RGBA{0, 0, 170, 255}
-	case "45":
-		currentBG = &color.RGBA{170, 0, 170, 255}
-	case "46":
-		currentBG = &color.RGBA{0, 255, 255, 255}
-	case "47":
-		currentBG = &color.RGBA{170, 170, 170, 255}
+	case "40", "100":
+		if bright || mode[0] == '1' {
+			currentBG = brightColors[0]
+		} else {
+			currentBG = basicColors[0]
+		}
+	case "41", "101":
+		if bright || mode[0] == '1' {
+			currentBG = brightColors[1]
+		} else {
+			currentBG = basicColors[1]
+		}
+	case "42", "102":
+		if bright || mode[0] == '1' {
+			currentBG = brightColors[2]
+		} else {
+			currentBG = basicColors[2]
+		}
+	case "43", "103":
+		if bright || mode[0] == '1' {
+			currentBG = brightColors[3]
+		} else {
+			currentBG = basicColors[3]
+		}
+	case "44", "104":
+		if bright || mode[0] == '1' {
+			currentBG = brightColors[4]
+		} else {
+			currentBG = basicColors[4]
+		}
+	case "45", "105":
+		if bright || mode[0] == '1' {
+			currentBG = brightColors[5]
+		} else {
+			currentBG = basicColors[5]
+		}
+	case "46", "106":
+		if bright || mode[0] == '1' {
+			currentBG = brightColors[6]
+		} else {
+			currentBG = basicColors[6]
+		}
+	case "47", "107":
+		if bright || mode[0] == '1' {
+			currentBG = brightColors[7]
+		} else {
+			currentBG = basicColors[7]
+		}
 	case "49":
 		currentBG = nil
 	default:
@@ -147,8 +202,39 @@ func (t *Terminal) handleColorMode(mode string, bright bool) {
 	}
 }
 
-func (t *Terminal) handleColorModeMap(mode, id string) {
-	log.Println("Unsupported grahics map id", id)
+func (t *Terminal) handleColorModeMap(mode, ids string) {
+	var c color.Color
+	id, err := strconv.Atoi(ids)
+	if err != nil {
+		log.Println("Invalid color map ID", ids)
+		return
+	}
+	if id <= 7 {
+		c = basicColors[id]
+	} else if id <= 15 {
+		c = brightColors[id-8]
+	} else if id <= 231 {
+		inc := 256/5
+		id -= 16
+		b := id % 6
+		id = (id-b) / 6
+		g := id % 6
+		r := (id-g) / 6
+		c = &color.RGBA{uint8(r*inc), uint8(g*inc), uint8(b*inc), 255}
+	} else if id <= 255 {
+		id -= 232
+		inc := 256/24
+		y := id*inc
+		c = &color.Gray{uint8(y)}
+	} else {
+		log.Println("Invalid colour map id", id)
+	}
+
+	if mode == "38" {
+		currentFG = c
+	} else if mode == "48" {
+		currentBG = c
+	}
 }
 
 func (t *Terminal) handleColorModeRGB(mode, rs, gs, bs string) {
