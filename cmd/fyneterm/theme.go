0f55e133be5eb2007656e031e50c8ca7c04c8f02
Andy Williams <andy@andywilliams.me>
2021-07-27 10:25:32 +0100

    Force dark theme for the terminal so all text is visible

--- cmd/fyneterm/theme.go
+++ cmd/fyneterm/theme.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/theme"
 )
@@ -10,7 +12,12 @@ type termTheme struct {
 }
 
 func newTermTheme() fyne.Theme {
-	return &termTheme{theme.DefaultTheme()}
+	return &termTheme{theme.DarkTheme()}
+}
+
+// Color fixes a bug < 2.1 where theme.DarkTheme() would not override user preference.
+func (t *termTheme) Color(n fyne.ThemeColorName, _ fyne.ThemeVariant) color.Color {
+	return t.Theme.Color(n, theme.VariantDark)
 }
 
 func (t *termTheme) Size(n fyne.ThemeSizeName) float32 {
