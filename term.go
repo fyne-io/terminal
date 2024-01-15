519d2c6f3d263c91c4096cda685a2415e7b0bc4b
figuerom16 <figuerom16@gmail.com>
2024-01-14 18:34:42 -0800

    Fix window size change monitoring

--- term.go
+++ term.go
@@ -322,9 +322,9 @@ func (t *Terminal) RunWithConnection(in io.WriteCloser, out io.Reader, windowCha
 	// monitor window size changes and run windowChangeFunc
 	ch := make(chan Config)
 	go func() {
-		<-ch
 		mr, mc := uint(0), uint(0)
 		for {
+			<-ch
 			r, c := t.config.Rows, t.config.Columns
 			if mr == r && mc == c {
 				continue
