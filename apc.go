603fb0cb0a3f3053c8f280294a11ec5961ebf074
Mark Gascoyne <contact@markgascoyne.co.uk>
2024-03-04 10:16:59 +0000

    Remove specific APC handlers

--- apc.go
+++ apc.go
@@ -1,25 +1,14 @@
 package terminal
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"os/exec"
-	"path"
-	"runtime"
 	"strings"
-
-	"github.com/fyne-io/terminal/internal/printer"
 )
 
 // APCHandler handles a APC command for the given terminal.
 type APCHandler func(*Terminal, string)
 
-var apcHandlers = map[string]func(*Terminal, string){
-	"set printer /windows-queue:": setWindowsQueue,
-	"set printer:_file:":          setFilePrinting,
-	"set printer:_editor:":        setEditorPrinting,
-}
+var apcHandlers = map[string]func(*Terminal, string){}
 
 func (t *Terminal) handleAPC(code string) {
 	for apcCommand, handler := range apcHandlers {
@@ -35,80 +24,6 @@ func (t *Terminal) handleAPC(code string) {
 	if t.debug {
 		// Handle other APC sequences or log the received APC code
 		log.Println("Unrecognised APC", code)
-	}
-}
-
-func setWindowsQueue(t *Terminal, arg string) {
-	t.printer = PrinterFunc(func(data []byte) {
-		// Write data to the printer
-		err := printer.PrintPostScriptFile(arg, data)
-		if err != nil && t.debug {
-			log.Println("Error printing postscript", err)
-			return
-		}
-	})
-}
-
-func setFilePrinting(t *Terminal, arg string) {
-	t.printer = PrinterFunc(func(data []byte) {
-		// Write data to the file
-		err := writeToFile(arg, data)
-		if err != nil && t.debug {
-			log.Println("Error writing to file", err)
-			return
-		}
-	})
-}
-
-// setEditorPrinting sets the printer to the printing editor.
-func setEditorPrinting(t *Terminal, arg string) {
-	t.printer = PrinterFunc(func(data []byte) {
-		tempDir, err := os.MkdirTemp("", "print-data")
-		if err != nil && t.debug {
-			log.Println("Error creating temporary directory", err)
-		}
-		_, file := path.Split(arg)
-		filename := path.Join(tempDir, file)
-		// Write data to the file
-		err = writeToFile(filename, data)
-		if err != nil && t.debug {
-			log.Println("Error writing to file", err)
-			return
-		}
-		// Open the default application for the file
-		err = openDefaultApp(filename)
-		if err != nil && t.debug {
-			log.Println("Error opening default application", err)
-		}
-	})
-}
-
-// Function to write data to a file
-func writeToFile(filename string, data []byte) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.Write(data)
-	return err
-}
-
-// Function to open the default application for a file
-func openDefaultApp(filename string) error {
-	switch runtime.GOOS {
-	case "linux":
-		return exec.Command("xdg-open", filename).Run()
-	case "windows":
-		return exec.Command("cmd", "/c", "start", filename).Run()
-	case "darwin":
-		return exec.Command("open", filename).Run()
-	case "android":
-		// Assuming you have a shell on the Android device
-		return exec.Command("am", "start", "-a", "android.intent.action.VIEW", "-d", "file://"+filename).Run()
-	default:
-		return fmt.Errorf("unsupported operating system: %s", runtime.GOOS)
 	}
 }
 
--- internal/printer/printer_unix.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package printer
-
-// PrintPostScriptFile writes the filedata to the printer.
-func PrintPostScriptFile(printerName string, filedata []byte) error {
-	// TODO implement me.
-	return nil
-}
